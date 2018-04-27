c22ff9e71d183a48de5aebe2f8d33c88d88901b7
Daniel Björkander <danielb@wip.se>
2018-04-27 14:34:03 +0200

    Changed some packges

--- template/src/main.js
+++ template/src/main.js
@@ -1,9 +1,11 @@
 import 'es6-promise/auto'
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
+Vue.use(Vuelidate);
 
 new Vue({
   el: '#app',
